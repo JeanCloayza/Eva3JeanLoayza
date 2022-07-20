5c971ded669c7f192c6bc79863a3b4cd143533bb
jeanCl loayza <ferloje@hotmail.com>
2022-07-20 00:49:33 -0500

    examen3

---
+++ src/main/java/com/idat/ReservarCita/Ec2JeanLoayzaHospitalReservapApplication.java
@@ -0,0 +1,13 @@
+package com.idat.ReservarCita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2JeanLoayzaHospitalReservapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2JeanLoayzaHospitalReservapApplication.class, args);
+	}
+
+}
